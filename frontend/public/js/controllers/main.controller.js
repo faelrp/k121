340c6f9d161502488216968e90a9bc512e1178cf
Rafael Bertelli <rbertelli@commandalkon.com>
2017-12-27 17:48:24 -0200

    Improved responsiveness

--- frontend/public/js/controllers/main.controller.js
+++ frontend/public/js/controllers/main.controller.js
@@ -5,8 +5,8 @@
         .module('app')
         .controller('MainController', Controller);
 
-    Controller.$inject = ['$transitions'];
-    function Controller($transitions) {
+    Controller.$inject = ['$transitions', '$state'];
+    function Controller($transitions, $state) {
         var vm = this;
 
         vm.selectedItem = "";
@@ -16,5 +16,7 @@
         $transitions.onSuccess({}, function(trans) {
             vm.selectedItem = trans.to().data.selectedItem;
         });
+
+        $state.go('pessoas');
     }
 })();
